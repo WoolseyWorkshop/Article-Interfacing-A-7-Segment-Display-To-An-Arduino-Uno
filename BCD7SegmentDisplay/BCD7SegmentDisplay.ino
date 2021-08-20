2d188b5616c7493573947e3a8dc55eaf68b90436
John Woolsey <3237461+woolseyj@users.noreply.github.com>
2021-08-20 16:57:38 -0400

    Added bypass capacitor to schematic and removed ISR from program.

--- BCD7SegmentDisplay/BCD7SegmentDisplay.ino
+++ BCD7SegmentDisplay/BCD7SegmentDisplay.ino
@@ -6,75 +6,56 @@
 // reset to 0 when a button is pressed.
 //
 // Circuit:
-// Momentary push button connected to pin D2.
-// Common anode 7-segment display connected through 74LS47 IC to pins D3-D6.
+// - A momentary push button is connected to pin D2.
+// - A common anode 7-segment display is connected to pins D3-D6 through a
+//   74LS47 IC.
 //
 // Created by John Woolsey on 01/30/2019.
-// Copyright © 2019 Woolsey Workshop.  All rights reserved.
+// Updated by John Woolsey on 08/20/2021.
+// Copyright (c) 2019 Woolsey Workshop.  All rights reserved.
 
 
 // Pin Mapping
-const byte button = 2;  // momentary push button, fires interrupt service routine
-const byte bcdA   = 3;  // binary coded decimal (BCD) least significant bit (LSB) for 74LS47 A input pin
-const byte bcdB   = 4;
-const byte bcdC   = 5;
-const byte bcdD   = 6;  // binary coded decimal (BCD) most significant bit (MSB) for 74LS47 D input pin
+const uint8_t Button = 2;  // momentary push button
+const uint8_t BCD_A = 3;   // binary coded decimal (BCD) least significant bit (LSB) for 74LS47 A input pin
+const uint8_t BCD_B = 4;
+const uint8_t BCD_C = 5;
+const uint8_t BCD_D = 6;   // binary coded decimal (BCD) most significant bit (MSB) for 74LS47 D input pin
 
 
-// Global Variables
-unsigned long lastTimeButtonPressed = 0;  // used for debouncing button
-byte count = 0;                           // display counter
+// Global Constants
+const unsigned long DisplayPeriod = 1000;  // time between display updates in milliseconds
 
 
 void setup() {
    // Pin configuration
-   pinMode(button, INPUT_PULLUP);  // utilize microprocessor's internal pull-up resistor
-   pinMode(bcdA, OUTPUT);
-   pinMode(bcdB, OUTPUT);
-   pinMode(bcdC, OUTPUT);
-   pinMode(bcdD, OUTPUT);
-
-   // Initialize interrupt service routine
-   // Calls resetCount() function when button is pressed,
-   // i.e., the button pin value falls from high to low.
-#ifdef ARDUINO_AVR_UNO_WIFI_REV2
-   attachInterrupt(button, resetCount, FALLING);
-#else
-   attachInterrupt(digitalPinToInterrupt(button), resetCount, FALLING);
-#endif
+   pinMode(Button, INPUT_PULLUP);  // utilize microprocessor's internal pull-up resistor
+   pinMode(BCD_A, OUTPUT);
+   pinMode(BCD_B, OUTPUT);
+   pinMode(BCD_C, OUTPUT);
+   pinMode(BCD_D, OUTPUT);
 }
 
 
 void loop() {
-   displayWrite(count);         // update display
-   count++;                     // increase counter
-   if (count == 10) count = 0;  // reset to 0 if count exceeds 9
-   delay(1000);                 // wait one second
-}
+   static uint8_t count = 0;  // display count
+   static unsigned long previousDisplayTime = 0;
+   unsigned long currentTime = millis();
 
-
-// Resets counter and display to 0
-void resetCount() {
-   unsigned long currentTimeButtonPressed = millis();  // get current time
-   if (currentTimeButtonPressed - lastTimeButtonPressed > 250) {  // allow for button debounce time of 250 ms
-      lastTimeButtonPressed = currentTimeButtonPressed;
-      count = 0;        // reset counter
-      displayWrite(0);  // reset display
+   if (digitalRead(Button) == LOW) count = 0;  // reset count if button pressed
+   if (currentTime - previousDisplayTime >= DisplayPeriod) {  // check if time to update display
+      displayWrite(count);  // update display
+      count++;  // increase counter
+      if (count == 10) count = 0;  // reset to 0 if count exceeds 9
+      previousDisplayTime = currentTime;
    }
 }
 
 
 // Writes value to display using binary coded decimal
-void displayWrite(byte value) {
-#ifdef ARDUINO_AVR_UNO_WIFI_REV2
-   digitalWrite(bcdA, PinStatus(bitRead(value, 0)));  // BCD LSB
-   digitalWrite(bcdB, PinStatus(bitRead(value, 1)));
-   digitalWrite(bcdC, PinStatus(bitRead(value, 2)));
-   digitalWrite(bcdD, PinStatus(bitRead(value, 3)));  // BCD MSB
-#else
-   digitalWrite(bcdA, bitRead(value, 0));  // BCD LSB
-   digitalWrite(bcdB, bitRead(value, 1));
-   digitalWrite(bcdC, bitRead(value, 2));
-   digitalWrite(bcdD, bitRead(value, 3));  // BCD MSB
-#endif
+void displayWrite(uint8_t value) {
+   digitalWrite(BCD_A, bitRead(value, 0));  // BCD LSB
+   digitalWrite(BCD_B, bitRead(value, 1));
+   digitalWrite(BCD_C, bitRead(value, 2));
+   digitalWrite(BCD_D, bitRead(value, 3));  // BCD MSB
 }
